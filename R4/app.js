8ccd9adb6a9cafd19d8326190dd6505388c09ccb
Matias Mitre <62855102+MatiMitre@users.noreply.github.com>
2021-05-28 12:35:58 -0300

    Update app.js
    eliminacion de constantes inutilizadas

--- R4/app.js
+++ R4/app.js
@@ -1,13 +1,12 @@
 const key = "f53ebe50248f78e141ef6fedcafd3106";
 const table = document.getElementById("table");
-const tr = document.getElementsByTagName("tr");
 
 document.addEventListener("DOMContentLoaded", () => {
   fetchData();
 });
 
 const fetchData = () => {
-  let url = `http://api.mediastack.com/v1/news?access_key=f53ebe50248f78e141ef6fedcafd3106&keywords=clima&limit=8`;
+  let url = `http://api.mediastack.com/v1/news?access_key=${key}&keywords=clima&limit=8`;
   fetch(url)
     .then((data) => data.json())
     .then((res) => {
@@ -16,10 +15,7 @@ const fetchData = () => {
 };
 
 const printTbody = (res) => {
-  //console.log(res.data.length);
   for (let i = 0; i < res.data.length; i++) {
-    //console.log(res.data[i]);
-
     table.innerHTML += `<tr>
     <td>${res.data[i].author}</td>
     <td>${res.data[i].title}</td>
@@ -33,3 +29,4 @@ const printTbody = (res) => {
 function pares() {
   $("td:even").css("background", "grey");
 }
+
